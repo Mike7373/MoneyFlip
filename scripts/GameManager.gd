7d3e7d4f1d0f40e5de32c1937d5245237166b2c7
Mike <mic.bellotti02@gmail.com>
2024-06-10 15:37:50 +0200

    add game manager's main functions

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -1,1 +1,33 @@
 extends Node
+
+var currentRound:int = 1
+var isPlayerTurn:bool
+var isLastPlayer:bool = false
+
+func _ready():
+	isPlayerTurn = (bool)(randi() % 2)
+
+func PlayRound():
+	#qui ci va il collegamento al testo per mostrare quale round inizia + timer + sfx
+	if isPlayerTurn:
+		#apri UI della scelta dei sacchetti
+		pass
+	else:
+		#fai giocare l'AI
+		pass
+
+func EndRound():
+	if isLastPlayer:
+		isLastPlayer = false
+	else:
+		isLastPlayer = true
+		currentRound += 1
+	PlayRound()
+	
+func PlayGame():
+	#select character
+	PlayRound()
+
+func EndGame():
+	#schermata di sconfitta
+	pass
